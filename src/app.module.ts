2718eb3c01211bc4771cc2cf344842483484a32e
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-09 19:40:15 +0900

    ローカルデータベースへの接続設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
