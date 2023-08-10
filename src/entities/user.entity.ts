bb3af729919b5df5c0b801198363251f1ba32e07
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-10 17:39:35 +0900

    マイグレーションファイルを作成し実行

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    username: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+}
