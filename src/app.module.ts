f653759512e7875aaf221d0d2c6a67604c0c6634
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-10 17:39:23 +0900

    実行するための用意

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
-      entities: [],
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'postgres',
+    //   database: 'postgres',
+    //   entities: ['dist/entities/*.entity.js'],
+    //   migrations: ['dist/migrations/*.js'],
+    //   synchronize: true,
+    // }),
     AuthModule,
+    DatabaseModule,
   ],
 })
 export class AppModule { }
