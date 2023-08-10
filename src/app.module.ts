23ac4933a6ac1538e8725a513b49744b5485344c
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-10 14:26:23 +0900

    コマンドによる自動生成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [],
       synchronize: true,
     }),
+    AuthModule,
   ],
 })
 export class AppModule { }
