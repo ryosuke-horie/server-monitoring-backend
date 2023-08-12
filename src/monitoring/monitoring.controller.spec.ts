57b5f2c0afedf9dd04b24c994c99c1fab6aaf48b
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-12 16:11:13 +0900

    依存関係を修正して定義されていることのテストを通す

--- src/monitoring/monitoring.controller.spec.ts
+++ src/monitoring/monitoring.controller.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MonitoringController } from './monitoring.controller';
+import { MonitoringService } from './monitoring.service';
 
 describe('MonitoringController', () => {
   let controller: MonitoringController;
+  let mockMonitoringService;
 
   beforeEach(async () => {
+    mockMonitoringService = {
+      create: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MonitoringController],
+      providers: [
+        { provide: MonitoringService, useValue: mockMonitoringService },
+      ],
     }).compile();
 
     controller = module.get<MonitoringController>(MonitoringController);
