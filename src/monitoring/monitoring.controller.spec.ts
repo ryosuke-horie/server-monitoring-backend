17ef012c1d8eb291ddea2e045495b50fa7b515b9
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-12 16:35:44 +0900

    createメソッドのテストコードを作成

--- src/monitoring/monitoring.controller.spec.ts
+++ src/monitoring/monitoring.controller.spec.ts
@@ -24,4 +24,39 @@ describe('MonitoringController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('create', () => {
+    it('should create a monitoring record and return it', async () => {
+      const currentDate = new Date().toISOString();  // Date型をISO文字列に変換
+      
+      const mockMonitoringRecord = {
+        id: 1,
+        target_name: 'Test Name',
+        target_ip: '127.0.0.1',
+        is_backup_completed: true,
+        is_not_alert: false,
+        is_working: true,
+        created_at: currentDate,
+        updated_at: currentDate,
+      };
+      mockMonitoringService.create.mockResolvedValue(mockMonitoringRecord);
+
+      const createMonitoringDto = {
+        target_name: 'Test Name',
+        target_ip: '127.0.0.1',
+        is_backup_completed: true,
+        is_not_alert: false,
+        is_working: true,
+        created_at: currentDate,
+        updated_at: currentDate,
+      };
+
+      const user = { id: 1, username: 'Test User', email: 'test@example.com', password: 'test1234', monitorings: []};
+
+      const result = await controller.create(createMonitoringDto, user);
+
+      expect(mockMonitoringService.create).toHaveBeenCalledWith(createMonitoringDto, user);
+      expect(result).toEqual(mockMonitoringRecord);
+    });
+  });
 });
