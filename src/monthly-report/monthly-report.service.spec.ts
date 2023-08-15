9617b66ce7510d854742858f21f806dbeb35891f
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-16 01:17:03 +0900

    テストコードを記述

--- src/monthly-report/monthly-report.service.spec.ts
+++ src/monthly-report/monthly-report.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MonthlyReportService } from './monthly-report.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Monitoring } from '../entities/monitoring.entity';
 
 describe('MonthlyReportService', () => {
   let service: MonthlyReportService;
+  let mockMonitoringRepository = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MonthlyReportService],
+      providers: [
+        MonthlyReportService,
+        {
+          provide: getRepositoryToken(Monitoring),
+          useValue: mockMonitoringRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<MonthlyReportService>(MonthlyReportService);
