2ab8723b6595b227ff8479db5c9bac5684337063
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-17 20:30:53 +0900

    cors設定を変更

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://server-monitoring-prototype.vercel.app/',
+      'https://server-monitoring-prototype.vercel.app',
     ],
   });
 
