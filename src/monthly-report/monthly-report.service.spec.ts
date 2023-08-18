9a141967aa64873d0dd4f452b3963dfbe1d65b8a
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-18 10:24:39 +0900

    テストの中身を合わせて修正

--- src/monthly-report/monthly-report.service.spec.ts
+++ src/monthly-report/monthly-report.service.spec.ts
@@ -10,6 +10,13 @@ describe('MonthlyReportService', () => {
   beforeEach(async () => {
     mockMonitoringRepository = {
       find: jest.fn(),
+      createQueryBuilder: jest.fn(() => ({
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        andWhere: jest.fn().mockReturnThis(),
+        select: jest.fn().mockReturnThis(),
+        getMany: jest.fn(),
+      })),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -29,41 +36,73 @@ describe('MonthlyReportService', () => {
     expect(service).toBeDefined();
   });
 
-  describe('getMonthlyReport', () => {
-    it('should return the monthly report', async () => {
-      const dateYear = '202308';
-      const mockData = [
-        {
-          target_name: 'パチンコビスタ',
-          is_backup_completed: false,
-          is_not_alert: false,
-          is_working: false,
-          record_date: '2023/08/15',
-          userId: 1,
-        },
-      ];
-
-      // あらかじめモックリポジトリのfindメソッドが期待通りのデータを返すように設定
-      mockMonitoringRepository.find.mockImplementation((criteria) => {
-        if (criteria.where.target_name === 'パチンコビスタ') {
-          return Promise.resolve(mockData);
-        }
-        return Promise.resolve([]); // その他のターゲット名は空の配列を返す
-      });
-
-      const result = await service.getMonthlyReport(dateYear);
-
-      expect(mockMonitoringRepository.find).toBeCalled(); // findが呼び出されたかチェック
-      // 期待通りのデータが返されるかチェック
-      expect(result).toEqual({
-        パチンコビスタ: mockData,
-        エフエス: [],
-        券売機: [],
-        券売機プロ: [],
-        グループセッション: [],
+  describe('MonthlyReportService', () => {
+    let service: MonthlyReportService;
+    let mockMonitoringRepository;
+    let mockQueryBuilder;
+  
+    beforeEach(async () => {
+      mockQueryBuilder = {
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        andWhere: jest.fn().mockReturnThis(),
+        select: jest.fn().mockReturnThis(),
+        getMany: jest.fn(),
+      };
+  
+      mockMonitoringRepository = {
+        find: jest.fn(),
+        createQueryBuilder: jest.fn(() => mockQueryBuilder),
+      };
+  
+      const module: TestingModule = await Test.createTestingModule({
+        providers: [
+          MonthlyReportService,
+          {
+            provide: getRepositoryToken(Monitoring),
+            useValue: mockMonitoringRepository,
+          },
+        ],
+      }).compile();
+  
+      service = module.get<MonthlyReportService>(MonthlyReportService);
+    });
+  
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+    });
+  
+    describe('getMonthlyReport', () => {
+      it('should return the monthly report', async () => {
+        const dateYear = '202308';
+        const mockData = [
+          {
+            target_name: 'パチンコビスタ',
+            is_backup_completed: false,
+            is_not_alert: false,
+            is_working: false,
+            record_date: '2023/08/15',
+            user: {
+              username: 'testuser',
+            },
+          },
+        ];
+  
+        mockQueryBuilder.getMany.mockResolvedValueOnce(mockData).mockResolvedValueOnce([]).mockResolvedValue([]);
+  
+        const result = await service.getMonthlyReport(dateYear);
+  
+        expect(mockMonitoringRepository.createQueryBuilder).toBeCalled();
+        expect(mockQueryBuilder.getMany).toHaveBeenCalledTimes(5);
+        expect(result).toEqual({
+          パチンコビスタ: mockData,
+          エフエス: [],
+          券売機: [],
+          券売機プロ: [],
+          グループセッション: [],
+        });
       });
     });
   });
-
-  // getMonthlyTargetReportに関するテストも同様に追加できます。
+  
 });
