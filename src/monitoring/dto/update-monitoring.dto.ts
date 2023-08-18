e1a82263a9e82df8424a56a9728ba2455b34cab2
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-19 00:30:40 +0900

    updateメソッド用のdtoファイルを定義

---
+++ src/monitoring/dto/update-monitoring.dto.ts
@@ -0,0 +1,24 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateMonitoringDto {
+  @IsString()
+  @IsNotEmpty()
+  target_name: string;
+
+  @IsNotEmpty()
+  is_backup_completed: string;
+
+  @IsNotEmpty()
+  is_not_alert: string;
+
+  @IsNotEmpty()
+  is_working: string;
+
+  @IsNotEmpty()
+  @IsString()
+  record_date: string;
+
+  @IsString()
+  @IsNotEmpty()
+  updated_at: string;
+}
