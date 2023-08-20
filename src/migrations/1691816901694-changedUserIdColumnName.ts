7179be65038499095eb9cfb18447d6390bf325f3
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-20 09:22:41 +0900

    設定上不要ファイルになっているため削除

--- src/migrations/1691816901694-changedUserIdColumnName.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class ChangedUserIdColumnName1691816901694
-  implements MigrationInterface
-{
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
--- src/migrations/1692077994282-addRecordDate.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class AddRecordDate1692077994282 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
